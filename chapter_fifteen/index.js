9a9cc9da228ef4e4af680460e59e76a647be2130
BulBUl-Ahmed <mdbulbulmolla1222@gmail.com>
2022-05-28 19:02:19 +0600

    es6 practice

---
+++ chapter_fifteen/index.js
@@ -0,0 +1,31 @@
+var s = `fjjjjrjrj
+erjre 
+rerrj   jgjj
+              gtitttuurert`
+              console.log(s);
+
+var age = 13
+var name = 'Bulbul Ahmed'
+console.log(`My name is ${name} and I am ${age} years old. I am ${age < 18 ? 'not': ''} adult`);  
+
+console.log(name.padStart(40, ' *'));
+console.log(name.padEnd(20, ' a'));
+console.log('B'.repeat(15)); 
+
+// var vs let vs const
+
+// let a = 10
+// a = 100
+
+// console.log(a);
+
+if (true) {
+  let a = 10
+}
+// console.log(a);
+
+for (let i = 0; i < 7; i++) {
+
+}
+
+console.log(i);
