6def2dfddf5dbc27f22fca8c3f5dc77d6409a40a
BulBUl-Ahmed <mdbulbulmolla1222@gmail.com>
2022-06-12 16:31:29 +0600

    added error handling with try catch in javascript

--- chapter_sixteen/app.js
+++ chapter_sixteen/app.js
@@ -3,13 +3,31 @@
 //   throw new RangeError('N is Greater than 5')
 // }
 
-function changeToInt(v) {
-  let result = Number.parseInt(v) 
-  if (!result) {
-  return  'Please provide a value which is able to convert in Integer'
-  }
-  return result
-}
+// function changeToInt(v) {
+//   let result = Number.parseInt(v) 
+//   if (!result) {
+//   return  'Please provide a value which is able to convert in Integer'
+//   }
+//   return result
+// }
  
-let result = changeToInt('hfgiuf')
-console.log(result); 
+// let result = changeToInt('hfgiuf')
+
+function makeWords(text) {
+
+  try {
+    let str = text.trim()
+    let words = str.split(' ')
+    return words
+  } catch (e) { 
+    // console.log(e.message);
+    console.log('please provide a valid text');
+  }
+  let str = text.trim()
+  let words = str.split(' ')
+
+  return words
+}
+
+let words = makeWords(44)
+console.log(words);
