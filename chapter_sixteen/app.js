32062cd4e70f8bd080f03f730e71e2c2584dd09b
BulBUl-Ahmed <mdbulbulmolla1222@gmail.com>
2022-06-12 16:09:07 +0600

    added error handling with if else

--- chapter_sixteen/app.js
+++ chapter_sixteen/app.js
@@ -1,5 +1,15 @@
-let n = 10
-if (n > 5) {
-  throw new RangeError('N is Greater than 5')
+// let n = 10
+// if (n > 5) {
+//   throw new RangeError('N is Greater than 5')
+// }
+
+function changeToInt(v) {
+  let result = Number.parseInt(v) 
+  if (!result) {
+  return  'Please provide a value which is able to convert in Integer'
+  }
+  return result
 }
  
+let result = changeToInt('hfgiuf')
+console.log(result); 
