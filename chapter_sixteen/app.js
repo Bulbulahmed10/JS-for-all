1b9f6f9adc17575f14cef2c4abbaa32c2b9be52a
BulBUl-Ahmed <mdbulbulmolla1222@gmail.com>
2022-06-12 17:36:15 +0600

    added custom error in javascript

--- chapter_sixteen/app.js
+++ chapter_sixteen/app.js
@@ -32,15 +32,27 @@
 // let words = makeWords(44)
 // console.log(words);
 
+class CustomError extends Error {
+  constructor(msg) {
+    
+    super(msg)
+
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, CustomError)
+    }
+  }
+}
+
 try {
   console.log('I am line 1');
-  throw new Error('I am your Error');
+  throw new CustomError('I am your Error');
   console.log('I am line 2');
   console.log('I am line 3');
 
 } catch (e) {
-  console.log(e.message);
+  console.log(e);
+  console.dir(e);
 } finally {
   console.log('I am finally block');
 }
-  
+    
