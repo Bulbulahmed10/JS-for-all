58539236013328fd712a6744575212dd4208ba3b
BulBUl-Ahmed <mdbulbulmolla1222@gmail.com>
2022-06-28 10:06:43 +0600

    added ajax with callback in javascript

--- chapter_seventeen/index.js
+++ chapter_seventeen/index.js
@@ -10,22 +10,60 @@
 //  let output = sayMyName('Bulbul Ahmed')
 //  console.log(output);
 
-AJAX
+// AJAX
 
-Asynchronous Javascript and XML (JSON)
+// Asynchronous Javascript and XML (JSON)
 
-const xhr = new XMLHttpRequest();
-xhr.open('GET', "https://jsonplaceholder.typicode.com/users");
+// const xhr = new XMLHttpRequest();
+// xhr.open('GET', "https://jsonplaceholder.typicode.com/users");
 
-xhr.onload = function (e) {
-  // if (xhr.readyState === 4) {
-  //   if(xhr.status === 200) {
-  //     console.log(xhr.response);
-  //   } else {
-  //     console.log(xhr.response);
-  //   }
-  // }
-  console.log(xhr.response);
-};
+// xhr.onload = function (e) {
+//   if (xhr.readyState === 4) {
+//     if(xhr.status === 200) {
+//       let users = JSON.parse(xhr.response)
+//       console.log(users);
+//     } else {
+//       console.log(xhr.response);
+//     }
+//   }
+  
+// };
 
-xhr.send();
+// xhr.send();
+
+function getRequest(url, callback) {
+  const xhr = new XMLHttpRequest()
+  xhr.open('GET', url);
+
+  xhr.onload = function (e) {
+    if (xhr.readyState === 4) {
+      if(xhr.status === 200) {
+        let response = JSON.parse(xhr.response)
+        callback(null, response)
+      } else {
+        callback(xhr.status, null)
+      }
+    }
+    
+  }
+  xhr.send()
+}
+
+getRequest('https://jsonplaceholder.typicode.com/users', (err, res) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(res);
+    res.forEach(b => alert(b.name))
+  }
+})
+
+getRequest('https://jsonplaceholder.typicode.com/posts', (err, res) => {
+  if (err) {
+    console.log(err);
+  } else {
+    res.forEach(r => console.log(r.title))
+  }
+})
+
+
