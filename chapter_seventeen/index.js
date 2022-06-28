284034ebcd8938613be7b5280b438b1f3ac40e65
BulBUl-Ahmed <mdbulbulmolla1222@gmail.com>
2022-06-28 10:30:07 +0600

    callback not always asyncronous in javascript

--- chapter_seventeen/index.js
+++ chapter_seventeen/index.js
@@ -31,39 +31,55 @@
 
 // xhr.send();
 
-function getRequest(url, callback) {
-  const xhr = new XMLHttpRequest()
-  xhr.open('GET', url);
+// function getRequest(url, callback) {
+//   const xhr = new XMLHttpRequest()
+//   xhr.open('GET', url);
 
-  xhr.onload = function (e) {
-    if (xhr.readyState === 4) {
-      if(xhr.status === 200) {
-        let response = JSON.parse(xhr.response)
-        callback(null, response)
-      } else {
-        callback(xhr.status, null)
-      }
-    }
+//   xhr.onload = function (e) {
+//     if (xhr.readyState === 4) {
+//       if(xhr.status === 200) {
+//         let response = JSON.parse(xhr.response)
+//         callback(null, response)
+//       } else {
+//         callback(xhr.status, null)
+//       }
+//     }
     
-  }
-  xhr.send()
+//   }
+//   xhr.send()
+// }
+
+// getRequest('https://jsonplaceholder.typicode.com/users', (err, res) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(res);
+//     res.forEach(b => alert(b.name))
+//   }
+// })
+
+// getRequest('https://jsonplaceholder.typicode.com/posts', (err, res) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     res.forEach(r => console.log(r.title))
+//   }
+// })
+
+let arr = [1, 2, 3, 4]
+
+let sqrArr = arr.map(v => v*v)
+
+console.log(sqrArr);
+
+function asyncMap(arr, cb) {
+  return arr.map(v => {
+    // setTimeout(cb.bind(null, v), 0)
+    setTimeout(() => cb(v), 0)
+  })
 }
 
-getRequest('https://jsonplaceholder.typicode.com/users', (err, res) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(res);
-    res.forEach(b => alert(b.name))
-  }
+let qbArr = asyncMap(arr, v => {
+  console.log(v)
 })
-
-getRequest('https://jsonplaceholder.typicode.com/posts', (err, res) => {
-  if (err) {
-    console.log(err);
-  } else {
-    res.forEach(r => console.log(r.title))
-  }
-})
-
-
+console.log(qbArr)
